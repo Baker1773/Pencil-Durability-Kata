0d732b1b67898a445c789a3e8dfaea470cf0ac38
Baker1773 <baker.1774@gmail.com>
2018-02-07 19:15:25 -0500

    Added test for writting text with insufficent lead resulting to unintentional white space being written Moved test from PaperTest into PencilTest as the test tested logic in pencil

--- src/PencilTest.java
+++ src/PencilTest.java
@@ -81,4 +81,59 @@ public class PencilTest {
 
 		assertEquals(4, pencil.GetLength());
 	}
+
+	@Test
+	public void PaperGetsAppleWrittenOnIt() {
+		Pencil pencil = new Pencil(20, 5);
+
+		Paper paper = new Paper();
+		pencil.Write("Apple", paper);
+
+		assertEquals("Apple", paper.GetText());
+
+	}
+
+	@Test
+	public void PaperGetsOnionWrittenOnIt() {
+		Pencil pencil = new Pencil(20, 5);
+
+		Paper paper = new Paper();
+		pencil.Write("Onion", paper);
+
+		assertEquals("Onion", paper.GetText());
+
+	}
+
+	@Test
+	public void PaperGetsSentenceWrittenOnIt() {
+		Pencil pencil = new Pencil(20, 5);
+
+		Paper paper = new Paper();
+		pencil.Write("This is a sentence", paper);
+
+		assertEquals("This is a sentence", paper.GetText());
+
+	}
+
+	@Test
+	public void PaperGetsSentenceWithNewLineWrittenOnIt() {
+		Pencil pencil = new Pencil(20, 5);
+
+		Paper paper = new Paper();
+		pencil.Write("This is a sentence\n", paper);
+
+		assertEquals("This is a sentence\n", paper.GetText());
+
+	}
+
+	@Test
+	public void InsufficientLeadAttemptsTextButWritesTex() {
+		Pencil pencil = new Pencil(4, 5);
+
+		Paper paper = new Paper();
+		pencil.Write("Text", paper);
+
+		assertEquals("Tex ", paper.GetText());
+
+	}
 }
