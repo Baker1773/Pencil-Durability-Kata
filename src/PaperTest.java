02558ea402f9c40a4f7140d2875ed0edaa083d50
Baker1773 <baker.1774@gmail.com>
2018-02-08 19:52:36 -0500

    When paper is created paper text is empty

--- src/PaperTest.java
+++ src/PaperTest.java
@@ -1,4 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class PaperTest {
 
+	@Test
+	public void WhenPaperIsCreatedItIsBlank() {
+		Paper paper = new Paper();
+		assertEquals("", paper.text);
+	}
 }
